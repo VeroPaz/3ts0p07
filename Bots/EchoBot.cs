dcc75ac16ea878446f55e6febd82e9fddcebcc00
VeroPaz <veronica.paz.88@outlook.com>
2019-07-26 10:00:42 -0400

    subiendo waterfall

--- Bots/EchoBot.cs
+++ Bots/EchoBot.cs
@@ -28,9 +28,7 @@ namespace EchoBotTest.Bots
         protected readonly Dialog Dialog;
         protected readonly ILogger Logger;        
 
-        //private const string WelcomeMessage = @"Holi";
-        //private String nombre;       
-
+        
         public EchoBot(ConversationState conversationState, UserState userState, T dialog, ILogger<EchoBot<T>> logger)
         {
             ConversationState = conversationState;
@@ -44,7 +42,7 @@ namespace EchoBotTest.Bots
           
             await base.OnTurnAsync(turnContext, cancellationToken);
 
-            // Save any state changes that might have occured during the turn.
+            // GUARDA LOS CAMBIOS DE ESTADO QUE HAN DURANTE LOS TURNOS
             await ConversationState.SaveChangesAsync(turnContext, false, cancellationToken);
             await UserState.SaveChangesAsync(turnContext, false, cancellationToken);
         }
@@ -54,120 +52,8 @@ namespace EchoBotTest.Bots
                         
             Logger.LogInformation("Running dialog with Message Activity.");
 
-            // Run the Dialog with the new message Activity.
+            // EJECUTA EL DIALOGO CON EL NUEVO MENSAJE DE ACTIVIDAD
             await Dialog.RunAsync(turnContext, ConversationState.CreateProperty<DialogState>(nameof(DialogState)), cancellationToken);
-        }
-
-        //protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
-        //{
-
-        //    await SendWelcomeMessageAsync(turnContext, cancellationToken); //va al método SendWelcomeMessageAsync
-
-        //}
-
-        //protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
-        //{
-
-        //    var conversationStateAccessors = ConversationState.CreateProperty<ConversationData>(nameof(ConversationData));
-        //    var conversationData = await conversationStateAccessors.GetAsync(turnContext, () => new ConversationData());
-        //    var userStateAccessors = ConversationState.CreateProperty<UserProfile>(nameof(UserProfile));
-        //    var userProfile = await userStateAccessors.GetAsync(turnContext, () => new UserProfile());
-
-
-
-        //    if (string.IsNullOrEmpty(userProfile.Name))//verificar que la variable Name se encuentra vacia
-        //    {
-
-        //        //Primera vez es falso, por lo que se solicita nombre del usuario
-        //        if (userProfile.PromptedUserForName) //Variable boolean iniciada en false --> ConversationData.cs
-        //        {
-        //            // Establece el nombre, variable nombre, clase conversationData
-        //            userProfile.Name = turnContext.Activity.Text?.Trim();
-
-        //            // Reconoce el nombre, y envía una nueva indicación
-        //            await turnContext.SendActivityAsync($"Gracias {userProfile.Name}. Por favor ahora ingresa tu E-Mail");
-
-        //            // Reinicia "flag"
-        //            userProfile.PromptedUserForName = false;
-        //        }
-        //        else
-        //        {
-        //            await turnContext.SendActivityAsync($"Bienvenido a Bot Crear Ticket Service?");
-
-        //            // Al ser PromptedUserForName falso, pregunta su nombre
-        //            await turnContext.SendActivityAsync($"Ingresar nuevo ticket?");
-
-        //            // flag boolean true, inicia ciclo de if con el ingreso del nombre del usuario.
-        //            userProfile.PromptedUserForName = true; //cambiar a consultar por continuar
-        //        }
-        //    }
-        //    else
-        //    {
-        //        // Add message details to the conversation data.
-        //        // Convert saved Timestamp to local DateTimeOffset, then to string for display.
-        //        var messageTimeOffset = (DateTimeOffset)turnContext.Activity.Timestamp;
-        //        var localMessageTime = messageTimeOffset.ToLocalTime();
-        //        conversationData.Timestamp = localMessageTime.ToString();
-        //        conversationData.ChannelId = turnContext.Activity.ChannelId.ToString();
-        //        //userProfile.Mail = turnContext.Activity.From.
-        //        userProfile.Mail = turnContext.Activity.Text?.Trim();
-        //        userProfile.Name = turnContext.Activity.From.Name;
-
-        //        //var connector = new ConnectorClient(new Uri(turnContext.Activity.ServiceUrl));
-        //        //var teamId = turnContext.Activity.GetChannelData<TeamsChannelData>().Team.Id;
-        //        //var members = await connector.Conversations.GetConversationMembersAsync(teamId);
-        //        //var member1 in members.AsTeamsChannelAccounts();
-        //        //WindowsIdentity nombre = WindowsIdentity.GetCurrent();
-
-        //        // Display state data.
-        //        await turnContext.SendActivityAsync($"Tu Nombre es:{userProfile.Name}");
-        //        await turnContext.SendActivityAsync($"Tu e-mail: {userProfile.Mail}");
-        //        await turnContext.SendActivityAsync($"Tu e-mail: {userProfile.Mail2}");
-        //        await turnContext.SendActivityAsync($"Message received at: {conversationData.Timestamp}");
-        //        await turnContext.SendActivityAsync($"Message received from: {conversationData.ChannelId}");
-
-        //        //acá agregar algo para pasar a otra clase para hacer un flujo de conversación....
-        //    }
-
-        //}
-
-        //private static async Task SendWelcomeMessageAsync(ITurnContext turnContext, CancellationToken cancellationToken)
-        //{
-        //    foreach (var member in turnContext.Activity.MembersAdded)
-        //    {
-        //        if (member.Id != turnContext.Activity.Recipient.Id)
-        //        {
-        //            await turnContext.SendActivityAsync(
-        //                $"Bienvenido a Bot Crear Ticket Service {member.Name}",
-        //                cancellationToken: cancellationToken);
-        //            await SendSuggestedActionsAsync(turnContext, cancellationToken);
-        //        }
-        //    }
-        //}
-
-        //private static async Task SendSuggestedActionsAsync(ITurnContext turnContext, CancellationToken cancellationToken)
-        //{
-        //    var reply = MessageFactory.Text("Que desea realizar?");
-                        
-        //    reply.SuggestedActions = new SuggestedActions()
-        //    {
-        //        Actions = new List<CardAction>()
-        //            {
-        //                new CardAction() { Title = "Red", Type = ActionTypes.ImBack, Value = "Red" },
-        //                new CardAction() { Title = "Yellow", Type = ActionTypes.ImBack, Value = "Yellow" },
-        //                new CardAction() { Title = "Blue", Type = ActionTypes.ImBack, Value = "Blue" },
-        //            },
-        //    };
-        //    await turnContext.SendActivityAsync(reply, cancellationToken);
-        //}
-
-        //public override async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
-        //{//manejo de la actividad entrante
-        //    await base.OnTurnAsync(turnContext, cancellationToken);
-
-        //    await ConversationState.SaveChangesAsync(turnContext, false, cancellationToken); //Guardar datos
-        //    // await UserState.SaveChangesAsync(turnContext, false, cancellationToken);
-        //}
-
+        }       
     }
 }
--- Dialogs/UserProfileDialog.cs
+++ Dialogs/UserProfileDialog.cs
@@ -41,19 +41,9 @@ namespace EchoBotTest.Dialogs
         }
 
         private static async Task<DialogTurnResult> TransportStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
-        {
-            //await SendWelcomeMessageAsync(stepContext, cancellationToken);
+        {            
             await stepContext.Context.SendActivityAsync(MessageFactory.Text("Bienvenido a Bot Crear Ticket Service"), cancellationToken);
-            return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = MessageFactory.Text("Por favor ingresa tu nombre.") }, cancellationToken);
-            // WaterfallStep always finishes with the end of the Waterfall or with another dialog; here it is a Prompt Dialog.
-            // Running a prompt here means the next WaterfallStep will be run when the users response is received.
-
-            //return await stepContext.PromptAsync(nameof(ChoicePrompt),
-            //    new PromptOptions
-            //    {
-            //        Prompt = MessageFactory.Text("Please enter your mode of transport."),
-            //        Choices = ChoiceFactory.ToChoices(new List<string> { "Car", "Bus", "Bicycle" }),
-            //    }, cancellationToken);
+            return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = MessageFactory.Text("Por favor ingresa tu nombre.") }, cancellationToken);                   
 
         }
 
@@ -68,48 +58,13 @@ namespace EchoBotTest.Dialogs
         {
             stepContext.Values["mail"] = (string)stepContext.Result;
 
-            // We can send messages to the user at any point in the WaterfallStep.
+            //PARA ENVIAR UN MENSAJE AL USUARIO........
             await stepContext.Context.SendActivityAsync(MessageFactory.Text($"Gracias "), cancellationToken);
-
-            // WaterfallStep always finishes with the end of the Waterfall or with another dialog; here it is a Prompt Dialog.
-            //return await stepContext.PromptAsync(nameof(ConfirmPrompt), new PromptOptions { Prompt = MessageFactory.Text("Would you like to give your age?") }, cancellationToken);
+                                    
             var msg = $"Tu mail es: {stepContext.Result} está correcto?";
             return await stepContext.PromptAsync(nameof(ConfirmPrompt), new PromptOptions { Prompt = MessageFactory.Text(msg) }, cancellationToken);
         }
 
-        //private async Task<DialogTurnResult> AgeStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
-        //{
-        //    if ((bool)stepContext.Result)
-        //    {
-        //        // User said "yes" so we will be prompting for the age.
-        //        // WaterfallStep always finishes with the end of the Waterfall or with another dialog, here it is a Prompt Dialog.
-        //        var promptOptions = new PromptOptions
-        //        {
-        //            Prompt = MessageFactory.Text("Please enter your age."),
-        //            RetryPrompt = MessageFactory.Text("The value entered must be greater than 0 and less than 150."),
-        //        };
-
-        //        return await stepContext.PromptAsync(nameof(NumberPrompt<int>), promptOptions, cancellationToken);
-        //    }
-        //    else
-        //    {
-        //        // User said "no" so we will skip the next step. Give -1 as the age.
-        //        return await stepContext.NextAsync(-1, cancellationToken);
-        //    }
-        //}
-
-        //private async Task<DialogTurnResult> ConfirmStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
-        //{
-        //    stepContext.Values["age"] = (int)stepContext.Result;
-
-        //    var msg = (int)stepContext.Values["age"] == -1 ? "No age given." : $"I have your age as {stepContext.Values["age"]}.";
-
-        //    // We can send messages to the user at any point in the WaterfallStep.
-        //    await stepContext.Context.SendActivityAsync(MessageFactory.Text(msg), cancellationToken);
-
-        //    // WaterfallStep always finishes with the end of the Waterfall or with another dialog, here it is a Prompt Dialog.
-        //    return await stepContext.PromptAsync(nameof(ConfirmPrompt), new PromptOptions { Prompt = MessageFactory.Text("Is this ok?") }, cancellationToken);
-        //}
 
         private async Task<DialogTurnResult> SummaryStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
@@ -118,33 +73,21 @@ namespace EchoBotTest.Dialogs
                 // Get the current profile object from user state.
                 var userProfile = await _userProfileAccessor.GetAsync(stepContext.Context, () => new UserProfile(), cancellationToken);
 
-                userProfile.Mail = (string)stepContext.Values["mail"];
+                userProfile.Mail = (string)stepContext.Values["mail"];                
                 
-                //userProfile.Mail2 = (int)stepContext.Values["age"];
-
                 var msg = $"Tu nombre es: {userProfile.Name} y tu mail es: {userProfile.Mail}.";
-                //if (userProfile.Age != -1)
-                //{
-                //    msg += $" And age as {userProfile.Age}.";
-                //}
+               
 
                 await stepContext.Context.SendActivityAsync(MessageFactory.Text(msg), cancellationToken);
             }
             else
             {
-                await stepContext.Context.SendActivityAsync(MessageFactory.Text("Thanks. Your profile will not be kept."), cancellationToken);
+                await stepContext.Context.SendActivityAsync(MessageFactory.Text("Los datos no serán guardados"), cancellationToken);
             }
 
-            // WaterfallStep always finishes with the end of the Waterfall or with another dialog, here it is the end.
+            // ESTA PARTE INDICA EL FINAL DEL DIALOGO (ÚLTIMO INDICADO EN LA WATERFALL
             return await stepContext.EndDialogAsync(cancellationToken: cancellationToken);
         }
-
-        
-
-        //private static Task<bool> AgePromptValidatorAsync(PromptValidatorContext<int> promptContext, CancellationToken cancellationToken)
-        //{
-        //    // This condition is our validation rule. You can also change the value at this point.
-        //    return Task.FromResult(promptContext.Recognized.Succeeded && promptContext.Recognized.Value > 0 && promptContext.Recognized.Value < 150);
-        //}
+                       
     }
 }
